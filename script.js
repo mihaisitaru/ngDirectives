e59b9fc3373ff47ed61398f15072d520f04c56b8
Mihai Sitaru <mihai.sitaru@gmail.com>
2018-06-01 22:16:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// Code goes here
+
+(function() {
+  
+    angular.module('app', [])
+          .controller('mainCtrl', function ($scope) {
+      $scope.message = 'Hello Plunker!';
+      $scope.user = {
+          name: 'Mihai Sitaru',
+          age: 31,
+          city: 'Iasi',
+          friends: [
+            'AB',
+            'BC',
+            'CD'
+            ]
+      }
+    })
+          .directive('userInfoCard', function () {
+            return {
+             templateUrl: "userInfoCard.html",
+             restrict: "E",
+             controller: function ($scope) {
+                 $scope.knightMe = function (user) {
+                  user.rank = 'Knight';
+                }
+                
+                $scope.unknightMe = function (user) {
+                  user.rank = '';
+                }
+                
+                $scope.card = true;
+                
+                $scope.hideCard = function () {
+                  $scope.card = false;
+                  $('.card-body').slideUp('slow').delay(100);
+                }
+                
+                $scope.showCard = function () {
+                  $scope.card = true;
+                  $('.card-body').slideDown('slow').delay(100);
+                }
+              }
+      }});
+  
+  })();
