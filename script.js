34fbea7ba976a063de7f23f7f6ac9789cceb380a
mihaisitaru <mihai.sitaru@yahoo.com>
2018-06-03 20:49:38 +0300

    Created second user info card

--- script.js
+++ script.js
@@ -1,49 +1,71 @@
 // Code goes here
 /* global $ */
 
-(function() {
+(function () {
 
     'use strict';
 
     angular.module('app', [])
-          .controller('mainCtrl', function ($scope) {
-      $scope.message = 'Hello Plunker!';
-      $scope.user = {
-          name: 'Mihai Sitaru',
-          age: 31,
-          city: 'Iasi',
-          friends: [
-            'AB',
-            'BC',
-            'CD'
-            ]
-      }
-    })
-          .directive('userInfoCard', function () {
+        .controller('mainCtrl', function ($scope) {
+            $scope.message = 'Hello Plunker!';
+            $scope.user1 = {
+                name: 'Mihai Sitaru',
+                age: 31,
+                city: 'Iasi',
+                friends: [
+                    'AB',
+                    'BC',
+                    'CD'
+                ]
+            };
+            $scope.user2 = {
+                name: 'Anamaria Sitaru',
+                age: 31,
+                city: 'Iasi',
+                friends: [
+                    'AB',
+                    'BC',
+                    'CD'
+                ]
+            };
+        })
+        .directive('userInfoCard', function () {
             return {
-             templateUrl: "userInfoCard.html",
-             restrict: "E",
-             controller: function ($scope) {
-                 $scope.knightMe = function (user) {
-                  user.rank = 'Knight';
+                templateUrl: "userInfoCard.html",
+                restrict: "E",
+                scope: {
+                    user: "="
+                },
+                controller: function ($scope) {
+                    $scope.knightMe = function (user) {
+                        user.rank = 'Knight';
+                    }
+
+                    $scope.unknightMe = function (user) {
+                        user.rank = '';
+                    }
+
+                    $scope.card = true;
+
+                    $scope.hideCard = function () {
+                        $scope.card = false;
+                        $('.card-body').slideUp('slow').delay(100);
+                    }
+
+                    $scope.showCard = function () {
+                        $scope.card = !$scope.card;
+                        $('.card-body').slideDown('slow').delay(100);
+                    }
                 }
-                
-                $scope.unknightMe = function (user) {
-                  user.rank = '';
-                }
-                
-                $scope.card = true;
-                
-                $scope.hideCard = function () {
-                  $scope.card = false;
-                  $('.card-body').slideUp('slow').delay(100);
-                }
-                
-                $scope.showCard = function () {
-                  $scope.card = true;
-                  $('.card-body').slideDown('slow').delay(100);
-                }
-              }
-      }});
-  
-  })();
+            }
+        });
+
+   /* .directive('myDirective', function () {
+        return {
+            scope: {
+
+            }
+        }
+    });*/
+
+})();
